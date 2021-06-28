5cf45524b59df13465e8d4918953e5b559ca2a9e
priyanka-whj <79097513+priyanka-whj@users.noreply.github.com>
2021-06-29 00:25:52 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,86 @@
+var platform, platformGroup;
+var mario;
+var wall;
+var obstacle;
+var marioAnimation, platformAnimation, wallAnimation, obstacleAnimation;
+
+function preload()
+{
+  marioAnimation = loadAnimation("images/Capture1.png", "images/Capture3.png", "images/Capture4.png");
+  platformAnimation = loadImage("images/ground.png");
+  wallAnimation = loadImage("images/wall.png");
+  obstacleAnimation = loadImage("images/obstacle1.png");
+}
+
+function setup() 
+{
+  createCanvas(displayWidth, 700);
+
+  var xDistance = 0;
+  var gap;
+  mario = new Player();
+  platformGroup = new Group();
+
+  for(var i=0; i<20; i++)
+  {
+    platform = new Platform(xDistance);
+    gap = random([50, 100]);
+    xDistance = xDistance + platform.sptW + gap;
+    platformGroup.add(platform.spt);
+    
+    if(i%3 === 0)
+    {
+      wall = new Wall(xDistance);
+      platformGroup.add(wall.spt);
+    }
+    if(i%4 === 0)
+    {
+      obstacle = new Obstacle(xDistance);
+    }
+  }
+}
+
+function draw() 
+{
+  background('skyblue');  
+
+  translate(-mario.spt.x + width/2, 0);
+
+  mario.applyGravity();
+  mario.spt.collide(platformGroup);
+
+  if(keyDown("left"))
+  {
+    mario.moveLeft();
+  }
+  if(keyDown("right"))
+  {
+    mario.moveRight();
+  }
+  if(keyDown("up") && mario.spt.velocityY === 0)
+  {
+    mario.jump();
+  }
+
+  drawSprites();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
